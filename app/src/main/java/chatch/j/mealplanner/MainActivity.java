b604151489dbfd204be16ce9701ab86dbe2e3b62
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-08-04 10:55:31 -0800

    add functionality to bottom navigation create 3 fragments for the bottom navigation

--- app/src/main/java/chatch/j/mealplanner/MainActivity.java
+++ app/src/main/java/chatch/j/mealplanner/MainActivity.java
@@ -1,9 +1,15 @@
 package chatch.j.mealplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,7 +25,77 @@ public class MainActivity extends AppCompatActivity {
 
         // Connect the components of the xml to the main activity
         toolbar = findViewById(R.id.toolbar);
-        mainBottomNavigationView = findViewById(R.id.mainBottomNavigationView);
+        mainBottomNavigationView = (BottomNavigationView) findViewById(R.id.mainBottomNavigationView);
+
+        // Attach the toolbar
         setSupportActionBar(toolbar);
+
+        // Set a default item to be selected and perform actions when items are clicked
+        setupNavigationView();
+    }
+
+    private void setupNavigationView(){
+
+        // Give the bottom navigation tabs action when clicked
+        if(mainBottomNavigationView != null){
+
+            // Select the first menu item by default and show proper fragment (TodayFragment)
+            Menu menu = mainBottomNavigationView.getMenu();
+            selectFragment(menu.getItem(0));
+
+            // Set action to perform when menu item is selected
+            mainBottomNavigationView.setOnNavigationItemSelectedListener(
+                    new BottomNavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                            selectFragment(menuItem);
+                            return false;
+                        }
+                    }
+            );
+        }
+    }
+
+    /**
+     * Perform action when any menu item is selected
+     * @param item item on menu that has been selected
+     */
+    protected void selectFragment(MenuItem item){
+        item.setChecked(true);
+
+        switch(item.getItemId()){
+            case R.id.todayMenuItem:
+                // Action to perform when Today menu item is selected
+                pushFragment(new TodayFragment());
+                break;
+            case R.id.calendarMenuItem:
+                // Action to perform when Calendar menu item is selected
+                pushFragment(new CalendarFragment());
+                break;
+            case R.id.recipesMenuItem:
+                //Action to perform when Recipes menu item is selected
+                pushFragment(new RecipesFragment());
+                break;
+        }
+    }
+
+    /**
+     * Method to push any fragment into given id
+     * @param fragment an instance of a fragment to show into
+     *                 the given id
+     */
+    protected void pushFragment(Fragment fragment){
+        if(fragment == null){
+            return;
+        }
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if(fragmentManager != null){
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if(ft != null){
+                ft.replace(R.id.rootLayout, fragment);
+                ft.commit();
+            }
+        }
     }
 }
