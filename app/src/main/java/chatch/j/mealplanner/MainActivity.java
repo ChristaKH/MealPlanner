2143ab6c0db1e379921b88d0c069e6a2576af3cf
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-08-03 17:38:15 -0800

    set the icon and text color of the bottom navigation bar

--- app/src/main/java/chatch/j/mealplanner/MainActivity.java
+++ app/src/main/java/chatch/j/mealplanner/MainActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private BottomNavigationView mainBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Connect the components of the xml to the main activity
         toolbar = findViewById(R.id.toolbar);
+        mainBottomNavigationView = findViewById(R.id.mainBottomNavigationView);
         setSupportActionBar(toolbar);
     }
 }
