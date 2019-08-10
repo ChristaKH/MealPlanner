7f2714d9763be4849455183e009bcd8dde93e0c2
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-08-09 17:55:32 -0800

    Add code for the getItem() and getCount() methods of the view pager adapter class

--- app/src/main/java/chatch/j/mealplanner/Adapters/RecipesViewPagerAdapter.java
+++ app/src/main/java/chatch/j/mealplanner/Adapters/RecipesViewPagerAdapter.java
@@ -1,4 +1,45 @@
 package chatch.j.mealplanner.Adapters;
 
-public class RecipesViewPagerAdapter {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import chatch.j.mealplanner.MealsFragment;
+
+/**
+ * This is the adapter for the ViewPager used in the recipes fragment.
+ * State pager was used for the possibility of many recipes being added in
+ * the future.
+ */
+public class RecipesViewPagerAdapter extends FragmentStatePagerAdapter {
+    private int numTabs;
+    public RecipesViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        numTabs = 4;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        MealsFragment mealsFragment = new MealsFragment();
+        position++;
+        Bundle bundle = new Bundle();
+        bundle.putString("Meals", "Fragment: " + position);
+        mealsFragment.setArguments(bundle);
+        return mealsFragment;
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return numTabs;
+    }
 }
