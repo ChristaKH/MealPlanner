0b732565e78bd98c91158de4092f6a8889ec3829
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-08-09 17:56:14 -0800

    Clear the recipes fragment class to prepare for better code

--- app/src/main/java/chatch/j/mealplanner/RecipesFragment.java
+++ app/src/main/java/chatch/j/mealplanner/RecipesFragment.java
@@ -22,73 +22,10 @@ import chatch.j.mealplanner.Models.Recipe;
  */
 public class RecipesFragment extends Fragment {
 
-    /*
-    private RecyclerView recipesRecyclerView;
-    private LinearLayoutManager recipeLayoutManager;
-
-    private ArrayList<Recipe> breakfastRecipes;
-    private ArrayList<Recipe> lunchRecipes;
-    private ArrayList<Recipe> dinnerRecipes;
-    private ArrayList<Recipe> dessertRecipes;
-    private ArrayList<Recipe> otherRecipes;
-    private ArrayList<ArrayList<Recipe>> allRecipes;*/
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recipes, container, false);
-
-        /*
-        // Connect recycler view to the xml
-        recipesRecyclerView = view.findViewById(R.id.recipesRecyclerView);
-
-        // Initialize all recipe array lists
-        breakfastRecipes = new ArrayList<Recipe>();
-        lunchRecipes = new ArrayList<Recipe>();
-        dinnerRecipes = new ArrayList<Recipe>();
-        dessertRecipes = new ArrayList<Recipe>();
-        otherRecipes = new ArrayList<Recipe>();
-        allRecipes = new ArrayList<ArrayList<Recipe>>();
-
-        // Fill recipe array lists with dummy values for now
-        // @TODO fill array lists with appropriate values based on actual recipes
-        Recipe tempRecipe = new Recipe();
-        for(int i = 0; i < 10; i++){
-            tempRecipe.setCategory(Recipe.Category.BREAKFAST);
-            breakfastRecipes.add(tempRecipe);
-
-            tempRecipe.setCategory(Recipe.Category.LUNCH);
-            lunchRecipes.add(tempRecipe);
-
-            tempRecipe.setCategory(Recipe.Category.DINNER);
-            dinnerRecipes.add(tempRecipe);
-
-            tempRecipe.setCategory(Recipe.Category.DESSERT);
-            dessertRecipes.add(tempRecipe);
-
-            tempRecipe.setCategory(Recipe.Category.OTHER);
-            otherRecipes.add(tempRecipe);
-        }
-
-        // Fill allRecipes with the recipes based on category
-        allRecipes.add(breakfastRecipes);
-        allRecipes.add(lunchRecipes);
-        allRecipes.add(dinnerRecipes);
-        allRecipes.add(dessertRecipes);
-        allRecipes.add(otherRecipes);
-
-        // initialize the recycler view adapter for the vertical (outer) recycler view
-        mRecipeRecyclerViewAdapter = new RecipeRecyclerViewAdapter(this.getActivity(), allRecipes);
-
-        // Define layout manager for vertical recycler view in recipe fragment
-        recipeLayoutManager = new LinearLayoutManager(this.getActivity(), RecyclerView.VERTICAL, false);
-
-        // Setup main(vertical) recycler view in the recipes fragment
-        recipesRecyclerView.setHasFixedSize(true);
-        recipesRecyclerView.setLayoutManager(recipeLayoutManager);
-        recipesRecyclerView.setAdapter(mRecipeRecyclerViewAdapter);
-        */
-        // Inflate the layout for this fragment
         return view;
     }
 }
