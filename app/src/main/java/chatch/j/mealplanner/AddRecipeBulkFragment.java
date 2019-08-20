8884a563f5ce462552d34cd7b1f9aa04bb54bf87
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-08-19 21:05:48 -0800

    Change position and length of edittexts for creator of recipe

--- app/src/main/java/chatch/j/mealplanner/AddRecipeBulkFragment.java
+++ app/src/main/java/chatch/j/mealplanner/AddRecipeBulkFragment.java
@@ -16,13 +16,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 
+import com.warkiz.widget.IndicatorSeekBar;
+
 
 /**
  * A simple {@link Fragment} subclass.
+ * This is a Fragment that takes in the bulk of the information for
+ * a new Recipe from the user.
+ * The information taken using this fragment includes:
+ *     - Recipe Title: Required (TextView)
+ *     - Creator: Optional (TextView)
+ *     - Cook Time: Optional (SeekBar)
+ *     - Difficulty: Optional (drop down menu, radio buttons, or seek bar)
+ *     - Image: Optional (normal button that opens up the gallery to take a picture)
+ *     - Category: Optional (drop down menu, radio buttons, or seek bar)
  */
 public class AddRecipeBulkFragment extends Fragment {
 
-    private SeekBar cookTimeSeekbar;
+    private IndicatorSeekBar cookTimeSeekbar;
 
     public AddRecipeBulkFragment() {
         // Required empty public constructor
@@ -33,6 +44,8 @@ public class AddRecipeBulkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_recipe_bulk, container, false);
+        cookTimeSeekbar = view.findViewById(R.id.cookTimeSeekBar);
+
         return view;
     }
 
