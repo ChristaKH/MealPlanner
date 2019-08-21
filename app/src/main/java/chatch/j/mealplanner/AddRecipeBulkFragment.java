e9b0c319a9914933286bc98c4eaf90eee20acd0e
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-08-20 20:29:24 -0800

    Connect first 3 components to their xml counterparts

--- app/src/main/java/chatch/j/mealplanner/AddRecipeBulkFragment.java
+++ app/src/main/java/chatch/j/mealplanner/AddRecipeBulkFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.SeekBar;
 
 import com.warkiz.widget.IndicatorSeekBar;
@@ -33,6 +34,9 @@ import com.warkiz.widget.IndicatorSeekBar;
  */
 public class AddRecipeBulkFragment extends Fragment {
 
+    // Important components from the xml
+    private EditText newRecipeTitleEditText;
+    private EditText newRecipeCreatorEditText;
     private IndicatorSeekBar cookTimeSeekbar;
 
     public AddRecipeBulkFragment() {
@@ -44,6 +48,10 @@ public class AddRecipeBulkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_recipe_bulk, container, false);
+
+        // Connect java objects to their xml counterparts
+        newRecipeTitleEditText = view.findViewById(R.id.newRecipeTitleEditText);
+        newRecipeCreatorEditText = view.findViewById(R.id.newRecipeCreatorEditText);
         cookTimeSeekbar = view.findViewById(R.id.cookTimeSeekBar);
 
         return view;
