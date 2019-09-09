77398bcd3ec8d6d01884d6f91ea6e323a4ae03e0
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-09-09 10:06:14 -0800

    Add to all recipe fragments so that they ae able to retrieve the Recipes from the database Add to all recipe fragments so that they can add specific recipes to the RecyclerViews Add to all recipe fragments so that if there are no recipes in their RecyclerViews they will display a message to indicate so

--- app/src/main/java/chatch/j/mealplanner/DrinksFragment.java
+++ app/src/main/java/chatch/j/mealplanner/DrinksFragment.java
@@ -4,18 +4,40 @@ package chatch.j.mealplanner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import chatch.j.mealplanner.Adapters.RecipeRecyclerViewAdapter;
+import chatch.j.mealplanner.Models.DBHelper;
+import chatch.j.mealplanner.Models.Recipe;
 
 
 /**
  * A subclass of the Fragment class that will be used to display
  * a list of recipes with the category of "Drink"
+ * Will display a message when there are no Drink Recipes in
+ * order to indicate so.
  */
 public class DrinksFragment extends Fragment {
 
+    // Components of the xml
+    private RecyclerView drinksRecyclerView;
+    private TextView noDrinksTextView;
+
+    // Adapter for the RecyclerView
+    private RecipeRecyclerViewAdapter adapter;
+
+    // Database related
+    private DBHelper db;
+    private ArrayList<Recipe> drinks;
+    private ArrayList<Recipe> allRecipes;
 
     public DrinksFragment() {
         // Required empty public constructor
@@ -26,7 +48,41 @@ public class DrinksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drinks, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_drinks, container, false);
 
+        // Initialize database related objects
+        db = new DBHelper(getContext());
+        drinks = new ArrayList<Recipe>();
+        allRecipes = (ArrayList<Recipe>) db.getAllRecipes();
+
+        // Connect to the xml components
+        noDrinksTextView = view.findViewById(R.id.noDrinksTextView);
+        drinksRecyclerView = view.findViewById(R.id.drinksRecyclerView);
+
+        // Initialize and attack adapter for the RecyclerView
+        adapter = new RecipeRecyclerViewAdapter(drinks);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        drinksRecyclerView.setLayoutManager(layoutManager);
+        drinksRecyclerView.setAdapter(adapter);
+        drinksRecyclerView.setHasFixedSize(true);
+
+        // Separate out all Recipes with the category of DRINK and add to the
+        // drinks ArrayList
+        Recipe tempRecipe;
+        for (int i = 0; i < allRecipes.size(); i++) {
+            tempRecipe = allRecipes.get(i);
+            if (tempRecipe.getCategory() == Recipe.Category.DRINK) {
+                drinks.add(tempRecipe);
+            }
+        }
+
+        // If there are no drink recipes then display the message to indicate so
+        if (drinks.size() == 0) {
+            noDrinksTextView.setVisibility(View.VISIBLE);
+        } else {
+            noDrinksTextView.setVisibility(View.INVISIBLE);
+        }
+
+        return view;
+    }
 }
--- app/src/main/java/chatch/j/mealplanner/OthersFragment.java
+++ app/src/main/java/chatch/j/mealplanner/OthersFragment.java
@@ -4,17 +4,41 @@ package chatch.j.mealplanner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import chatch.j.mealplanner.Adapters.RecipeRecyclerViewAdapter;
+import chatch.j.mealplanner.Models.DBHelper;
+import chatch.j.mealplanner.Models.Recipe;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A subclass of the Fragment class that will be used to display
+ * a list of recipes with the category of "Others"
+ * Will display a message when there are no Other Recipes in
+ * order to indicate so.
  */
 public class OthersFragment extends Fragment {
 
+
+    // Components of the xml
+    private RecyclerView othersRecyclerView;
+    private TextView noOthersTextView;
+
+    // Adapter for the RecyclerView
+    private RecipeRecyclerViewAdapter adapter;
+
+    // Database related
+    private DBHelper db;
+    private ArrayList<Recipe> others;
+    private ArrayList<Recipe> allRecipes;
 
     public OthersFragment() {
         // Required empty public constructor
@@ -25,7 +49,42 @@ public class OthersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_others, container, false);
+        View view = inflater.inflate(R.layout.fragment_others, container, false);
+
+        // Initialize database related objects
+        db = new DBHelper(getContext());
+        others = new ArrayList<Recipe>();
+        allRecipes = (ArrayList<Recipe>) db.getAllRecipes();
+
+        // Connect to the xml components
+        noOthersTextView = view.findViewById(R.id.noOthersTextView);
+        othersRecyclerView = view.findViewById(R.id.othersRecyclerView);
+
+        // Initialize and attack adapter for the RecyclerView
+        adapter = new RecipeRecyclerViewAdapter(others);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        othersRecyclerView.setLayoutManager(layoutManager);
+        othersRecyclerView.setAdapter(adapter);
+        othersRecyclerView.setHasFixedSize(true);
+
+        // Separate out all Recipes with the category of OTHER and add to the
+        // others ArrayList
+        Recipe tempRecipe;
+        for (int i = 0; i < allRecipes.size(); i++) {
+            tempRecipe = allRecipes.get(i);
+            if (tempRecipe.getCategory() == Recipe.Category.OTHER) {
+                others.add(tempRecipe);
+            }
+        }
+
+        // If there are no other recipes then display the message to indicate so
+        if (others.size() == 0) {
+            noOthersTextView.setVisibility(View.VISIBLE);
+        } else {
+            noOthersTextView.setVisibility(View.INVISIBLE);
+        }
+
+        return view;
     }
 
 }
