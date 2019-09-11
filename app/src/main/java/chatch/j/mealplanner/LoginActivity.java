c80e695cd5a00da0a28890e211320d4f82810345
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-09-10 18:40:27 -0800

    Begin adding and initializing Firebase object

--- app/src/main/java/chatch/j/mealplanner/LoginActivity.java
+++ app/src/main/java/chatch/j/mealplanner/LoginActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * This is the Login Activity which is used to allow the user to sign into their
  * existing accounting or to sign up with a new account. The user can only have one
@@ -12,9 +14,16 @@ import android.os.Bundle;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth; // Firebase object
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Instantiate our Firebase object
+        mAuth = FirebaseAuth.getInstance();
     }
 }
