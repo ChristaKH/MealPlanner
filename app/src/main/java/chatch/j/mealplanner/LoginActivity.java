8e2960b419c051b6dbd59aaf5817fd6f3c4fddb9
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-09-10 18:38:14 -0800

    Add comments to Login Activity to describe the activity's purpose Add user permission to manifest for internet

--- app/src/main/java/chatch/j/mealplanner/LoginActivity.java
+++ app/src/main/java/chatch/j/mealplanner/LoginActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * This is the Login Activity which is used to allow the user to sign into their
+ * existing accounting or to sign up with a new account. The user can only have one
+ * account per email. This activity utilizes Firebase for authentification and will
+ * give the user access to the Recipes that are save to the database under their account.
+ */
 public class LoginActivity extends AppCompatActivity {
 
     @Override
