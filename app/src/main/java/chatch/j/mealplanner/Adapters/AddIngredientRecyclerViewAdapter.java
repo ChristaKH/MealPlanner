55a6faccae75431d1807abd28ffd7cddfb0bb826
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-09-14 22:00:52 -0800

    Add summary of adapter

--- app/src/main/java/chatch/j/mealplanner/Adapters/AddIngredientRecyclerViewAdapter.java
+++ app/src/main/java/chatch/j/mealplanner/Adapters/AddIngredientRecyclerViewAdapter.java
@@ -1,4 +1,9 @@
 package chatch.j.mealplanner.Adapters;
 
+/**
+ * This is an adapter class for the RecyclerView that is used in the AddIngredientFragment.
+ * This adapter utilizes the single_ingredient_layout.xml to display the ingredients that are
+ * added by the user as well as allow the user to remove an ingredient that has already been added.
+ */
 public class AddIngredientRecyclerViewAdapter {
 }
