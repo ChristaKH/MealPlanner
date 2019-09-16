0e28d2591f0f183ce580ac495f10b8948017e7eb
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-09-15 17:47:45 -0800

    Add empty and full constructors for the Ingredients class Add emoty shells for the mutators of the Ingredient class

--- app/src/main/java/chatch/j/mealplanner/Models/Ingredient.java
+++ app/src/main/java/chatch/j/mealplanner/Models/Ingredient.java
@@ -8,6 +8,41 @@ package chatch.j.mealplanner.Models;
 public class Ingredient{
     private int mAmount;
     private Type mType;
+    private String mName;
 
     public enum Type {WHOLE, TSP,TBSP, CUP, QT, LB, GAL, PT}
+
+    /**
+     * Empty constructor that sets the default values for the class's attributes
+     */
+    public Ingredient(){
+        mAmount = 0;
+        mType = Type.WHOLE;
+        mName = "";
+    }
+
+    /**
+     * Full constructor that uses the mutator methods to change the instance variables
+     * while checking for valid input.
+     * @param amount    Amount of an ingredient that is needed. Cannot be negative.
+     * @param type  Measurement type used for the ingredient.
+     * @param name  Name of the current ingredient
+     */
+    public Ingredient(int amount, Type type, String name){
+        this.setAmount(amount);
+        this.setType(type);
+        this.setName(name);
+    }
+
+    public void setAmount(int newAmount){
+
+    }
+
+    public void setType(Type newType){
+
+    }
+
+    public void setName(String newName){
+
+    }
 }
